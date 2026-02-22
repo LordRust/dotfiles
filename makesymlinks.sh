28114588a5f8b0963df5090958f5447c836e8636
LordRust <jonas@cloud8.se>
2026-02-22 23:51:17 +0100

    quoting issues

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -12,26 +12,26 @@ files="bash_aliases bash_profile bash_logout bashrc inputrc emacs tmux.conf dirc
 if [[ $1 == 'xserver' ]] ; then
 	files="$files Xresources"
 fi
-echo $files ; exit
-
-##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+if [[ ! -d $olddir ]] ; then
+	echo "Creating $olddir for backup of any existing dotfiles in ~"
+	mkdir -p $olddir
+	echo "...done"
+fi
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir
+cd $dir || exit
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+		echo "Moving any existing dotfiles from ~ to $olddir"
+	if [[ -f $file ]] ; then
+		mv ~/."$file" ~/dotfiles_old/"$file"_"$(date +%Y%m%d%H%M)"
+		echo "Creating symlink to $file in home directory."
+	fi
 done
 
 ln -s $dir/freemem.sh ~/bin/freemem.sh

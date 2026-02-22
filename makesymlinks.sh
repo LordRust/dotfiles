acffeee8f1ce2ea840565d1210b49275ebe5cc34
LordRust <jonas@cloud8.se>
2026-02-23 00:13:50 +0100

    make symlinks safer

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,32 +6,44 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir="$HOME/dotfiles"                    # dotfiles directory
+olddir="$HOME/dotfiles_old"             # old dotfiles backup directory
 files="bash_aliases bash_profile bash_logout bashrc inputrc emacs tmux.conf dircolors gitconfig"    # list of files/folders to symlink in homedir
 if [[ $1 == 'xserver' ]] ; then
 	files="$files Xresources"
 fi
 
 # create dotfiles_old in homedir
-if [[ ! -d $olddir ]] ; then
+if [[ ! -d "$olddir" ]] ; then
 	echo "Creating $olddir for backup of any existing dotfiles in ~"
-	mkdir -p $olddir
+	mkdir -p "$olddir"
 	echo "...done"
 fi
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir || exit
+cd "$dir" || exit
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-		echo "Moving any existing dotfiles from ~ to $olddir"
-	if [[ -f $file ]] ; then
-		mv ~/."$file" ~/dotfiles_old/"$file"_"$(date +%Y%m%d%H%M)"
-		echo "Creating symlink to $file in home directory."
+	src="$dir/$file"
+	dst="$HOME/.$file"
+
+	if [[ ! -e "$src" ]]; then
+		echo "Skipping $file: source file not found."
+		continue
 	fi
+
+	if [[ -e "$dst" || -L "$dst" ]]; then
+		backup="$olddir/${file}_$(date +%Y%m%d%H%M%S)"
+		echo "Backing up $dst to $backup"
+		mv "$dst" "$backup"
+	fi
+
+	echo "Creating symlink $dst -> $src"
+	ln -snf "$src" "$dst"
 done
 
-ln -s $dir/freemem.sh ~/bin/freemem.sh
+mkdir -p "$HOME/bin"
+ln -snf "$dir/freemem.sh" "$HOME/bin/freemem.sh"
